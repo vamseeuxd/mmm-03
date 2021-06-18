32f5e6f1a0f4e50437958f2d8949eec5edf76736
vamseeuxd <vamsee.uxd@gmail.com>
2021-06-18 16:31:34 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h3>Monthly Money Manager</h3>
+        <p>Comming Soon...</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
