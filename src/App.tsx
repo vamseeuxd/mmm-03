58bf7c81af6d4b2b1378861d6217f8fe5507a499
vamseeuxd <vamsee.uxd@gmail.com>
2021-06-18 17:09:00 +0530

    Added Layout Navigator

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,29 @@
-import React from 'react';
-import './App.css';
+import React, {useState} from 'react';
+import 'ui-neumorphism/dist/index.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import './App.scss';
+import {Card, Dialog} from 'ui-neumorphism';
+import Footer from "./layout/Footer";
+import Header from "./layout/Header";
+import MainMenu from "./layout/MainMenu";
 
 function App() {
+  const [dark] = useState(false);
+  const [showMainMenu, setShowMainMenu] = useState(false);
+  const mainPageClasses = 'rounded-0 over-flow-off vw-100 vh-100 d-flex justify-content-center align-items-center flex-row';
+  const toggleMainMenu = () => {
+    setShowMainMenu(!showMainMenu);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <h3>Monthly Money Manager</h3>
-        <p>Comming Soon...</p>
-      </header>
-    </div>
+      <Card flat dark={dark} className={mainPageClasses}>
+        <Card rounded className='main-page-container'>
+          <Header toggleMainMenu={toggleMainMenu} dark={dark}/>
+          <Footer dark={dark}/>
+        </Card>
+        <Dialog persistent={true} className='main-menu-dialog' minWidth={300} visible={showMainMenu} onClose={() => setShowMainMenu(false)}>
+          <MainMenu toggleMainMenu={toggleMainMenu} dark={dark}/>
+        </Dialog>
+      </Card>
   );
 }
 
---
+++ src/layout/Header.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {IconButton, Subtitle2} from "ui-neumorphism";
+import Icon from "@mdi/react";
+import {mdiMenu} from "@mdi/js";
+
+export default function Header(props: { dark: boolean, toggleMainMenu: () => void }) {
+    const {dark, toggleMainMenu} = props;
+    const headerClasses = 'app-header fixed-top w-100';
+    return (
+        <div className={headerClasses}>
+            <div className='header-content w-100 d-flex justify-content-start align-items-center py-2 px-4'>
+
+                <IconButton onClick={() => toggleMainMenu()}
+                            text={false}
+                            className='header-menu-button'
+                            dark={dark} size='large' rounded>
+                    <Icon path={mdiMenu} size={1} color='var(--primary)'/>
+                </IconButton>
+
+                <Subtitle2 dark={dark} className='m-0 p-0 ms-3'>Monthly Money Manager</Subtitle2>
+            </div>
+        </div>
+    )
+}
